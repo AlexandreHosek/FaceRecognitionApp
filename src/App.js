00d2e7cfcb3205e5ef053ddf759fce08a811fea7
AlexandreHosek <alexandre.hosek@gmail.com>
2020-03-07 16:39:26 +0100

    added search bar and style fron end

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Navigation />
-       <Logo />
-       {/*<Logo />
-              <ImageLinkForm />
-              <FaceRecognition />*/}
+        <Particles 
+          className='particles'
+          params={particlesOptions} 
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition />*/}
       </div>
     );
   }
